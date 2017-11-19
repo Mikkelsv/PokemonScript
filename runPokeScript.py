4f997f7ac4dfa4732aaf2ea1e69278c48d335e58
Erik Liodden <eriklio@stud.ntnu.no>
2017-11-20 00:39:16 +0100

    balance press speed and duration
    bot-chan is weak and slow :(

--- runPokeScript.py
+++ runPokeScript.py
@@ -2,8 +2,8 @@ from pokemonScript import *
 import keyboard
 import time
 
-pressSpeed = 0.01 #Time between actions
-pressDuration = 0.02 #Duration of key pressed down
+pressSpeed = 0.01  # Time between actions
+pressDuration = 0.03  # Duration of key pressed down
 
 if __name__ == "__main__":
     print("Waiting for esc",end='\r')
